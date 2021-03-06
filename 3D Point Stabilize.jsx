a3e8c21ac6511d800cb63755223119c055eebe9e
JamesPerlman <jam.e.perl@gmail.com>
2021-03-05 17:26:33 -0800

    Add two-point stabilization

--- 3D Point Stabilize.jsx
+++ 3D Point Stabilize.jsx
@@ -12,7 +12,7 @@
 
     var layers = comp.selectedLayers;
 
-    if (layers.length !== 2/* && layers.length !== 3*/) {
+    if (layers.length !== 2 && layers.length !== 3) {
         alert(STR_PLEASE_SELECT);
         return;
     }
@@ -39,37 +39,48 @@
 
     comp.time = 0;
 
+    trackNull1.name = "Track Null 1";
+    trackNull2.name = "Track Null 2";
+
+    var pinNull1 = comp.layers.addNull();
+    pinNull1.name = "Pin Null 1";
+    pinNull1.transform.position.expression =
+        "trackNull1 = thisComp.layer(\"" + trackNull1.name + "\");\n" +
+        "trackNull1.toComp(trackNull1.transform.anchorPoint);";
+    pinNull1.transform.position.setValue(pinNull1.transform.position.valueAtTime(0, false));
+    pinNull1.transform.position.expression = "";
+    
+    // Set stabilizeLayer expressions
+    stabilizeLayer.transform.anchorPoint.expression =
+        "trackNull1 = thisComp.layer(\"" + trackNull1.name + "\");\n" +
+        "trackNull1.toComp(trackNull1.transform.anchorPoint);";
+
+    stabilizeLayer.transform.position.expression =
+        "pinNull2D = thisComp.layer(\"" + pinNull1.name + "\");\n" +
+        "pinNull2D.toComp(pinNull2D.transform.anchorPoint);";
+
+    stabilizeLayer.transform.scale.expression =
+        "trackNull1 = thisComp.layer(\"" + trackNull1.name + "\");\n" +
+        "cp = thisComp.activeCamera.transform.position;\n" +
+        "cp0 = cp.valueAtTime(0);\n" +
+        "np = trackNull1.transform.position;\n" +
+        "np0 = np.valueAtTime(0);\n" +
+        "d0 = length(cp0 - np0);\n" +
+        "d = length(cp - np);\n" +
+        "s = 100 * d / d0;\n" +
+        "0.01 * s * value;";
+
     if (trackNull2) {
-        // Set up double-point stabilization
-    } else {
-        // Set up single-point stabilization
-
-        pinNull = comp.layers.addNull();
-        pinNull.name = "Pin Null";
-        pinNull.transform.position.expression =
+        // Having two points allows for rotation stabilization
+        stabilizeLayer.transform.rotation.expression =
             "trackNull1 = thisComp.layer(\"" + trackNull1.name + "\");\n" +
-            "trackNull1.toComp(trackNull1.transform.anchorPoint);";
-        pinNull.transform.position.setValue(pinNull.transform.position.valueAtTime(0, false));
-        pinNull.transform.position.expression = "";
-        
-        // Set stabilizeLayer expressions
-        stabilizeLayer.transform.anchorPoint.expression =
-            "trackNull1 = thisComp.layer(\"" + trackNull1.name + "\");\n" +
-            "trackNull1.toComp(trackNull1.transform.anchorPoint);";
-
-        stabilizeLayer.transform.position.expression =
-            "pinNull2D = thisComp.layer(\"" + pinNull.name + "\");\n" +
-            "pinNull2D.toComp(pinNull2D.transform.anchorPoint);";
-
-        stabilizeLayer.transform.scale.expression =
-            "trackNull1 = thisComp.layer(\"" + trackNull1.name + "\");\n" +
-            "cp = thisComp.activeCamera.transform.position;\n" +
-            "cp0 = cp.valueAtTime(0);\n" +
-            "np = trackNull1.transform.position;\n" +
-            "np0 = np.valueAtTime(0);\n" +
-            "d0 = length(cp0 - np0);\n" +
-            "d = length(cp - np);\n" +
-            "s = 100 * d / d0;\n" +
-            "0.01 * s * value;\n";
+            "trackNull2 = thisComp.layer(\"" + trackNull2.name + "\");\n" +
+            "p1 = (t) => trackNull1.toComp(trackNull1.transform.anchorPoint, t);\n" +
+            "p2 = (t) => trackNull2.toComp(trackNull2.transform.anchorPoint, t);\n" +
+            "d0 = p2(0) - p1(0);\n" +
+            "a0 = Math.atan2(d0[1], d0[0]);\n" +
+            "d = p2(time) - p1(time);\n" +
+            "a = Math.atan2(d[1], d[0]);\n" +
+            "value - radiansToDegrees(a - a0);";
     }
 })();
