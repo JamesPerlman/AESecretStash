4d8801cc942e205afdb16b67e2322488a003413e
JamesPerlman <jam.e.perl@gmail.com>
2021-05-02 10:25:56 -0700

    wip: exporter functionality

--- MP Import Export.jsx
+++ MP Import Export.jsx
@@ -1,4 +1,11 @@
 (function (thisObj) {
+    function isSecurityPrefSet() {
+        return app.preferences.getPrefAsLong(
+            "Main Pref Section",
+            "Pref_SCRIPTING_FILE_NETWORK_SECURITY"
+        ) === 1;
+    }
+
     function canWriteFiles() {
         if (isSecurityPrefSet()) return true;
         
@@ -10,7 +17,7 @@
         return isSecurityPrefSet();
     }
 
-    function parseCSV(text) {
+    function csvToDict(text) {
         var delimiter = ",";
 
         var rows = text.split("\n");
@@ -30,9 +37,71 @@
 
         return result;
     }
+    
+    function dictToCSV(dict) {
+        var columns = [];
+        
+        // collect data as columns
+        for (var keyName in dict) {
+            if (dict.hasOwnProperty(keyName)) {
+                var column = [keyName];
+                var values = dict[keyName];
+                for (var i = 0; i < values.length; ++i) {
+                    column.push(values[i]);
+                }
+                columns.push(column);
+            }
+        }
+        
+        if (columns.length === 0) {
+            return "";
+        }
+        
+        // convert data to rows
+        var rows = [];
+        for (var i = 0; i < columns[0].length; ++i) {
+            var row = [];
+            for (var j = 0; j < columns.length; ++j) {
+                row.push("\"" + columns[j][i].toString() + "\"");
+            }
+            rows.push(row.join(","));
+        }
+        return rows.join("\n");
+    }
+
+    (function main() {
+        if (!canWriteFiles()) {
+            return;
+        }
+    
+        var activeItem = app.project.activeItem;
+        if (!activeItem) {
+            alert('No active selection.');
+            return;
+        }
+        
+        var selectedProperties = activeItem.selectedProperties;
+        var data = {};
+        
+        for (var i = 0; i < selectedProperties.length; ++i) {
+            var prop = selectedProperties[i];
+            data[prop.name] = [];
+            
+            for (var j = 1; j <= prop.numKeys; ++j) {
+                var value = prop.keyValue(j);
+                data[prop.name].push(value);
+            }
+        }
+        
+        var str = dictToCSV(data);
+        var projectPath = app.project.file.fsName;
+        var folder = projectPath.split("\\").slice(0,-1).join("\\");
+        var outputFile = folder + "\\output.csv";
+        writeFile(outputFile, str);
+    })();
 
     function readFileWithDialog() {
-        var srcFile = File.openDialog("Choose motion data file (CSV)","CSV:*.csv");
+        var srcFile = File.openDialog("Open motion data file (CSV)","CSV:*.csv");
         if (srcFile === null) {
             // Open Dialog was canceled
             return;
@@ -44,9 +113,22 @@
         return fileContent;
     }
 
+    function writeFileWithDialog(fileContent) {
+        if (!canWriteFiles()) {
+            alert("Unable to export.  Please allow filesystem permissions.");
+        }
+
+        var dstFile = File.saveDialog("Save motion data file (CSV)","CSV:*.csv");
+        
+        dstFile.encoding = "utf-8";
+        dstFile.open("w");
+        dstFile.write(fileContent);
+        dstFile.close();
+    }
+
     function importCamera() {
         var fileContent = readFileWithDialog();
-        var csvData = parseCSV(fileContent);
+        var csvData = csvToDict(fileContent);
         alert(csvData["Time"][30]);
     }
 
